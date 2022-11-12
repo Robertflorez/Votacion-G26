1167116507791570fc514b984b5985665f1188c3
rodriguez <rodisenrique73@gmail.com>
2022-11-12 15:22:15 -0500

    Se hicieron modificaciones a la interfaz de inicio, se crearon los html correspondientes para la presentacion de los candidatos y sus partidos.

--- project/VotaApp/src/main/java/com/edu/unab/controlador/VotappControlador.java
+++ project/VotaApp/src/main/java/com/edu/unab/controlador/VotappControlador.java
@@ -20,4 +20,44 @@ public class VotappControlador {
 	public String estadisticaApp() {
 		return "/inicio/estadistica";
 	}
+	
+	@GetMapping("/cand-1")
+	public String infoCand1() {
+		return "/info-cand/cand-1";
+	}
+	
+	@GetMapping("/cand-2")
+	public String infoCand2() {
+		return "/info-cand/cand-2";
+	}
+	
+	@GetMapping("/cand-3")
+	public String infoCand3() {
+		return "/info-cand/cand-3";
+	}
+	
+	@GetMapping("/cand-4")
+	public String infoCand4() {
+		return "/info-cand/cand-4";
+	}
+	
+	@GetMapping("/part-1")
+	public String infoPart1() {
+		return "/info-cand/part-cand-1";
+	}
+	
+	@GetMapping("/part-2")
+	public String infoPart2() {
+		return "/info-cand/part-cand-2";
+	}
+	
+	@GetMapping("/part-3")
+	public String infoPart3() {
+		return "/info-cand/part-cand-3";
+	}
+	
+	@GetMapping("/part-4")
+	public String infoPart4() {
+		return "/info-cand/part-cand-4";
+	}
 }
